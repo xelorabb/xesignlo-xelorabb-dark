4294006d4e08a8a67e5865c8d654ceabf0fe9a99
xelorabb <xelorabb@gmail.com>
2023-09-30 19:39:49 +0200

    add progress bar

---
+++ src/stories/atoms/ProgressBar.stories.ts
@@ -0,0 +1,68 @@
+import type { StoryObj, Meta } from '@storybook/html';
+import type { ProgressBarProps } from './ProgressBar';
+import { createProgressBar } from './ProgressBar';
+
+const meta = {
+  title: 'Atoms/ProgressBar',
+  tags: ['autodocs'],
+  render: (args) => {
+    return createProgressBar(args);
+  },
+  argTypes: {
+    value: { control: 'number' },
+    min: { control: 'number' },
+    max: { control: 'number' },
+    mode: {
+      control: 'select',
+      options: ['primary', 'secondary']
+    },
+    size: {
+      control: 'select',
+      options: {
+        'extra small': 'xs',
+        'small': 'sm', 
+        'medium': 'md', 
+        'large': 'lg'
+      }
+    },
+    postfix: { control: 'text' }
+  },
+} satisfies Meta<ProgressBarProps>;
+
+export default meta;
+type Story = StoryObj<ProgressBarProps>;
+
+export const PrimaryExtraSmall: Story = {
+  args: {
+    value: 2,
+    size: 'xs'
+  },
+};
+
+export const PrimarySmall: Story = {
+  args: {
+    value: 3,
+    size: 'sm'
+  },
+};
+
+export const PrimaryMedium: Story = {
+  args: {
+    value: 4,
+    size: 'md'
+  },
+};
+
+export const PrimaryLarge: Story = {
+  args: {
+    value: 4,
+    size: 'lg'
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    value: 5,
+    mode: 'secondary'
+  },
+};
---
+++ src/stories/atoms/ProgressBar.ts
@@ -0,0 +1,37 @@
+import '../../../dist/index.css';
+
+export interface ProgressBarProps {
+  value: number;
+  min?: number;
+  max?: number;
+  postfix?: string;
+  mode?: 'primary' | 'secondary';
+  size?: 'xs' | 'sm' | 'md' | 'lg';
+}
+
+export const createProgressBar = ({
+  value,
+  min = 0,
+  max = 100,
+  postfix = '%',
+  mode = 'primary',
+  size = 'md',
+}: ProgressBarProps) => {
+  let progressBar = document.createElement('div');
+  let progress = document.createElement('div');
+  let label = document.createElement('label');
+
+  label.innerHTML = `${value}${postfix}`;
+  progress.style.width = `${((value-min)/(max-min))*100}%`
+
+  progressBar.className = [
+    'progress-bar',
+    `progress-bar--${mode}`,
+    `progress-bar--${size}`
+  ].join(' ');
+
+  progressBar.appendChild(progress);
+  progressBar.appendChild(label);
+
+  return progressBar;
+};
